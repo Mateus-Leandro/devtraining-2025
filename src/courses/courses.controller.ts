ab87c21903eba9ff156174d4f5cfaf7b42a5ffab
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-08-05 20:07:32 -0300

    [feat] ✨: cria metodos no service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -9,9 +9,12 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly courseService: CoursesService) {}
+
   @Get()
   findAll() {
     return 'Listagem de cursos';
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './courses.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'NestJs',
+      description: 'Curso sobre fundamentos do framework NestJS',
+      tags: ['node.js', 'nestjs', 'javascript', 'typescript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    return this.courses.find((course) => course.id === id);
+  }
+
+  create(createCourseDTO) {
+    this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO) {
+    const existingCourse = this.findOne(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex((course) => course.id === id);
+      if (index === -1) return;
+
+      this.courses[index] = {
+        id,
+        ...updateCourseDTO,
+      };
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index === -1) return;
+
+    this.courses.splice(index, 1);
+  }
+}
