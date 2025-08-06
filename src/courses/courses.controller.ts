aa2dd5ff4e1fcc93f9d161ed4505a35f0957dc68
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-08-06 18:37:03 -0300

    [feat] ✨: cria dtos para criacao e atualizacao de courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -10,6 +10,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { createCourseDTO } from './dto/create-course.dto';
+import { updateCourseDTO } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -26,13 +28,13 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.courseService.create(body);
+  create(@Body() createCourseDTO: createCourseDTO) {
+    return this.courseService.create(createCourseDTO);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.courseService.update(+id, body);
+  update(@Param('id') id: string, @Body() updateCourseDTO: updateCourseDTO) {
+    return this.courseService.update(+id, updateCourseDTO);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
