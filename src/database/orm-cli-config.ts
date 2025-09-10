809df88f6e651871077ce52e5251c6693424c950
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-09-10 20:50:15 -0300

    [refactor] ♻️: configuracao para utilizacao de variaveis de ambiente

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1754954701771 } from 'src/migrations/1754954701771-CreateCoursesTable';
 import { CreateTagsTable1754956215781 } from 'src/migrations/1754956215781-CreateTagsTable';
 import { CreateCoursesTagsTable1756247056347 } from 'src/migrations/1756247056347-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTagsTable1756248136383 } from 'src/migrations/1756248136383-AddCoursesIdToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1756248848525 } from 'src/migrations/1756248848525-AddTagsIdToCoursesTagsTable';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
